54085cbe122fc89d8873bc2c617a0ec6ec386261
Civero <civ3ro@gmail.com>
2025-12-24 11:51:24 +0100

    added test scene.
    Next: Change rendering method to the layered one.

--- scratch/three.js
+++ scratch/three.js
@@ -6,7 +6,7 @@
 // By: Drago-Cuven <https://scratch.mit.edu/users/DragoCuven/>
 // License: MPL-2.0 and MIT
 
-// Started collaboratively December 2025
+// Started collaboratively 23 December 2025
 
 (async function(Scratch) {
   'use strict';
@@ -16,22 +16,72 @@
   }
 
   const vm = Scratch.vm;
+  const bT = Scratch.BlockType //is this useful?
+  const aT = Scratch.ArgumentType
 
-  const THREE = await Scratch.external.importModule('https://cdn.jsdelivr.net/npm/three@latest/build/three.module.min.js');
+  //const THREE = await Scratch.external.importModule('https://cdn.jsdelivr.net/npm/three@latest/build/three.module.min.js');
+  //const THREE = await Scratch.external.importModule('https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.min.js')
+  const THREE = await Scratch.external.importModule("https://esm.sh/three@0.180.0")
 
-  console.log(THREE);
+  //Might be a good idea to start an API
+  window._ThreeJS_ = {
+    THREE: THREE
+  }
+  console.log(window._ThreeJS_);
 
   class ThreeJS {
     getInfo() {
       return {
         id: 'turboThree',
         name: Scratch.translate('ThreeJS'),
+        color1: '',
+        color2: '',
+        color3: '',
+        blockIconURI: '',
+        menuIconURI: '',
+
         blocks: [
 
-        ]
+          {opcode: 'test', blockType: bT.COMMAND, text: "init a scene", arguments: {}}
+
+        ],
+        menus: {
+
+        }
       };
     }
-  }
 
+    async test() {
+const width = vm.renderer.canvas.width, height = vm.renderer.canvas.height
+
+const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( width, height );
+renderer.setAnimationLoop( animate )
+console.log(renderer)
+
+vm.renderer.addOverlay( renderer.domElement, "manual" ) // change to layered method
+
+function animate( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
+    }
+
+  }
   Scratch.extensions.register(new ThreeJS());
 })(Scratch);
