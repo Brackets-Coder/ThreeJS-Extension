0e754cea52c365cb5085ca7bd2f66c73c4d3925f
Civero <civ3ro@gmail.com>
2025-12-26 20:36:54 +0100

    i do not have this very clear, but i want to leave this commit.

--- scratch/three.js
+++ scratch/three.js
@@ -29,151 +29,74 @@
   //const THREE = await Scratch.external.importModule('https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.min.js')
   const THREE = await Scratch.external.importModule("https://esm.sh/three@0.180.0")
 
-  //Might be a good idea to start an API
-  window._ThreeJS_ = {
-    THREE: THREE
-  }
-  console.log(window._ThreeJS_);
-
-//made by Vadik1 - Thanks!
 const Skin = renderer.exports.Skin;
-class SimpleSkin extends Scratch.vm.renderer.exports.Skin {
-    constructor(id, renderer) {
-      super(id, renderer);
-      const gl = renderer.gl;
-      const texture = gl.createTexture();
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-      this._texture = texture;
-      this._nativeSize = renderer.getNativeSize();
-      this._boundOnNativeSizeChanged = this.onNativeSizeChanged.bind(this);
-      this._rotationCenter = [this._nativeSize[0] / 2, this._nativeSize[1] / 2];
-      renderer.on("NativeSizeChanged", this._boundOnNativeSizeChanged);
-      this.resizeCanvas();
-    }
-    dispose() {
-      renderer.removeListener(
-        "NativeSizeChanged",
-        this._boundOnNativeSizeChanged
-      );
-      if (this._texture) {
-        this._renderer.gl.deleteTexture(this._texture);
-        this._texture = null;
-      }
-      super.dispose();
-    }
-    get size() {
-      return this._nativeSize;
-    }
-    getTexture(scale) {
-      return this._texture || super.getTexture();
-    }
-    updateContent() {
-      const gl = this._renderer.gl;
-      gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
-      gl.bindTexture(gl.TEXTURE_2D, this._texture);
-      gl.texImage2D(
-        gl.TEXTURE_2D,
-        0,
-        gl.RGBA,
-        gl.RGBA,
-        gl.UNSIGNED_BYTE,
-        canvas
-      );
-      gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
-      this._silhouette.update(canvas);
-      this.emitWasAltered();
-    }
-    resizeCanvas() {
-      if (renderer.useHighQualityRender) {
-        canvas.width = renderer.canvas.width;
-        canvas.height = renderer.canvas.height;
-      } else {
-        canvas.width = this._nativeSize[0];
-        canvas.height = this._nativeSize[1];
-      }
-      runtime.startHats(`${extensionId}_whenCanvasResized`);
-      this.updateContent();
-    }
-    onNativeSizeChanged(event) {
-      this._nativeSize = event.newSize;
-      this._rotationCenter = [this._nativeSize[0] / 2, this._nativeSize[1] / 2];
-      this.resizeCanvas();
-    }
-}
-function addSimple3DLayer(publicApi) {
-  // Register new drawable group "simple3D"
-  // To undertsand how this patch works, first understand how those are interconnected:
-  // renderer._groupOrdering => renderer._layerGroups => renderer._drawList => renderer._allDrawables
-  let index = renderer._groupOrdering.indexOf("video");
-  renderer._groupOrdering.splice(index + 1, 0, "simple3D");
-  renderer._layerGroups["simple3D"] = {
-    groupIndex: 0,
-    drawListOffset: renderer._layerGroups["video"].drawListOffset,
-  };
-  for (let i = 0; i < renderer._groupOrdering.length; i++) {
-    renderer._layerGroups[renderer._groupOrdering[i]].groupIndex = i;
+
+class SimpleSkin extends Skin {
+  constructor(id, renderer) {
+    super(id, renderer);
+    const gl = renderer.gl;
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    this._texture = texture;
+    this._rotationCenter = [320, 180];
+    this._size = [640, 360];
   }
-
-  // Create drawable and skin
-  skinId = renderer._nextSkinId++;
-  const skin = new SimpleSkin(skinId, renderer);
-  renderer._allSkins[skinId] = skin;
-  drawableId = renderer.createDrawable("simple3D");
-  const drawable = renderer._allDrawables[drawableId];
-  renderer.updateDrawableSkinId(drawableId, skinId);
-
-  // Detect resizing
-  drawable.setHighQuality = function (...args) {
-    Object.getPrototypeOf(this).setHighQuality(...args);
-    this.skin.resizeCanvas();
-  };
-
-  // Support for SharkPool's Layer Control extension
-  drawable.customDrawableName = "Simple3D Layer";
-
-  if (!redraw) {
-    const drawOriginal = renderer.draw;
-    renderer.draw = function () {
-      if (this.dirty && redraw) redraw();
-      drawOriginal.call(this);
-    };
-  }
-
-  redraw = function () {
-    if (canvasDirty) {
-      skin.updateContent(canvas);
-      canvasDirty = false;
+  dispose() {
+    if (this._texture) {
+      this._renderer.gl.deleteTexture(this._texture);
+      this._texture = null;
     }
-  };
-  redraw();
-}
-function removeSimple3DLayer() {
-  renderer.destroyDrawable(drawableId, "simple3D");
-  renderer.destroySkin(skinId);
-
-  const index = renderer._groupOrdering.indexOf("simple3D");
-  if (index == -1) return;
-  const start = renderer._layerGroups["simple3D"].drawListOffset;
-  const end =
-    renderer._layerGroups[renderer._groupOrdering[index + 1]].drawListOffset;
-  if (start !== end) return;
-  renderer._groupOrdering.splice(index, 1);
-  delete renderer._layerGroups["simple3D"];
-  for (let i = 0; i < renderer._groupOrdering.length; i++) {
-    renderer._layerGroups[renderer._groupOrdering[i]].groupIndex = i;
+    super.dispose();
   }
-  publicApi.redraw = null;
+  set size(value) {
+    this._size = value;
+    this._rotationCenter = [value[0] / 2, value[1] / 2];
+  }
+  get size() {
+    return this._size;
+  }
+  getTexture(scale) {
+    return this._texture || super.getTexture(scale);
+  }
+  setContent(textureData) {
+    const gl = this._renderer.gl;
+    gl.bindTexture(gl.TEXTURE_2D, this._texture);
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      textureData
+    );
+    this.emitWasAltered();
+  }
 }
 
-let drawableId = null;
-let skinId = null;
-let canvasDirty = true;
-let redraw = null;
-addSimple3DLayer()
+let threeSkinId = null;
+let threeSkin = null;
+let threeDrawableId = null;
+
+const threeRenderer = new THREE.WebGLRenderer({ /*preserveDrawingBuffer: true, antialias: false*/ });
+threeRenderer.setClearColor(0x000000, 0);
+threeRenderer.setSize(480, 360);
+
+threeSkinId = renderer._nextSkinId++;
+threeSkin = new SimpleSkin(threeSkinId, renderer);
+renderer._allSkins[threeSkinId] = threeSkin;
+threeDrawableId = renderer.createDrawable("pen");
+renderer._allDrawables[threeDrawableId].customDrawableName = "Three Layer";
+renderer.updateDrawableSkinId(threeDrawableId, threeSkinId)
+
+window._ThreeJS_ = {
+  THREE: THREE,
+  get threeSkin() {return threeSkin},
+  get threeRenderer() {return threeRenderer}
+}
 
   class ThreeJS {
     getInfo() {
@@ -198,32 +121,37 @@ addSimple3DLayer()
     }
 
     async test() {
-const width = renderer.canvas.width, height = renderer.canvas.height
+const width = runtime.stageWidth, height = runtime.stageHeight;
 
 const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-camera.position.z = 1;
+camera.position.z = 5;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const geometry = new THREE.TorusKnotGeometry();
 const material = new THREE.MeshNormalMaterial();
 
 const mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
 
-const threeRenderer = new THREE.WebGLRenderer( { antialias: true } );
 threeRenderer.setSize( width, height );
+threeSkin.size = [width, height]
 threeRenderer.setAnimationLoop( animate )
-console.log(threeRenderer)
 
-renderer.addOverlay( threeRenderer.domElement, "manual" ) // change to layered method
+	threeRenderer.render( scene, camera );
+  await threeSkin.setContent(threeRenderer.domElement)
+  console.log(threeSkin)
+
+//renderer.addOverlay( threeRenderer.domElement, "manual" ) // change to layered method
 
 function animate( time ) {
 
 	mesh.rotation.x = time / 2000;
 	mesh.rotation.y = time / 1000;
+  mesh.position.z = 3 * Math.sin(time / 1000)
 
 	threeRenderer.render( scene, camera );
+  //how to update?
 
 }
     }
